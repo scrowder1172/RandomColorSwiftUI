57c020c17c2f146dd6dacf7be83f2bab7dceba70
Scott Crowder <scrowder@gmail.com>
2024-05-15 15:53:07 -0500

    Initial Commit

---
+++ RandomColorSwiftUI/RandomColorSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomColorSwiftUIApp.swift
+//  RandomColorSwiftUI
+//
+//  Created by SCOTT CROWDER on 5/15/24.
+//
+
+import SwiftUI
+
+@main
+struct RandomColorSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
